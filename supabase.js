80a8bea158cbaaa360b30bb32da42dbad4ce8c56
skeldrose <136121622+skeldrose@users.noreply.github.com>
2025-06-07 16:25:33 +0100

    Hooked V6 Bug Fix
    supabase.js missing from last push.

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+const { createClient } = supabase;
+
+const supabaseUrl = 'https://tsfpxhabxsaslsmfypypp.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZnB4aGFieHNhc2xzbWZ5cHBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzMDM4ODgsImV4cCI6MjA2NDg3OTg4OH0.3Pr6b4voB7VQMTVaTtI0dfMDqpPBC90f1c84n0LwvaU';
+
+window.supabase = createClient(supabaseUrl, supabaseAnonKey);
